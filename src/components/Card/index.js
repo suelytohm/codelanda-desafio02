7d6d474d55a3b04bb16abcdf373d64cbffaa6ba5
suelytohm <suelytohm.02@gmail.com>
2023-04-20 18:05:54 -0300

    chore: link do checkout

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,7 +6,9 @@ export const Card = ({title, img, preco}) => {
     <div className='card'>
       <img src={`./img/${img}.svg`} alt={"Imagem do produto " + title} loading="lazy" />
       <p>{title}</p>
-      <span><strong>R${preco}</strong></span>
+      {/* <span><strong></strong></span> */}
+      {/* <a target="_blank" rel="noreferrer" href='https://donate.stripe.com/test_eVa0336zTbI561W3ce'>R${preco}</a> */}
+      <a target="_blank" rel="noreferrer" href='https://pag.ae/7ZjDDm1jq'>R${preco}</a>
       
     </div>
   )
